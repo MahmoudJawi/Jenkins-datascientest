faab02dcc7b05e398b68ebf037524bc64e4a0145
Mahmoud Abdeljawad <mahmud.abdeljawad@gmail.com>
2025-10-26 15:12:04 +0000

    Edit for testing webhook

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "Fuck Datascientest again and again for their outdated lessons!!"}
+    return {"Hello": "Fuck Datascientest again and again and again for their outdated lessons!!"}
